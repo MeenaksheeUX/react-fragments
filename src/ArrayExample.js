f31cc460ee0a9a9b4a1b6cd2156854355f1fcec3
Meenakshee Verma <meenaksheeverma.mca@gmail.com>
2020-02-26 00:11:35 -0800

    first commit

---
+++ src/ArrayExample.js
@@ -0,0 +1,8 @@
+import React from "react";
+function ArrayExample() {
+  return [
+    <h2 key="alpha">Learning about fragments</h2>,
+    <p key="beta">fragments are super amazing</p>
+  ];
+}
+export default ArrayExample;
---
+++ src/Fragment.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Fragment() {
+  return <h2> Learning about fragments </h2>;
+}
+export default Fragment;
